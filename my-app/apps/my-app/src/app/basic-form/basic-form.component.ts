adab449922c947ba9a9ec5717aa12ecec0c23f4b
salehloke <58175520+salehloke@users.noreply.github.com>
2023-02-23 23:46:26 +0800

    2 files modified

--- my-app/apps/my-app/src/app/basic-form/basic-form.component.ts
+++ my-app/apps/my-app/src/app/basic-form/basic-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent {
+  mainFormGroup: FormGroup;
+  constructor(
+    /**
+     * - in order to use FormBuilder, need to import the ReactiveFormsModule & FormsModule
+     */
+    private fb: FormBuilder
+  ) {}
 
+  ngOnInit() {
+    // create and init the form
+    this.initForm();
+  }
+
+  initForm() {
+    this.mainFormGroup = this.fb.group({
+      name: [''],
+      age: 0,
+    });
+  }
 }
